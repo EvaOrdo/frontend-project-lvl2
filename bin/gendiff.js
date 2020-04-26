c5ab0cd37f508590f1ee3b594785d4dc57fb95fc
Hanna Mudrak <annmudrak@gmail.com>
2020-04-26 13:35:09 -0500

    step 2    add -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new commander.Command();
 
 program
   .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f --format [type]', 'output format');
 program.parse(process.argv);
