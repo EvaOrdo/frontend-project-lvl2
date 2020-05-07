cc619aff420084ab61c5e548022cfc0b83d918a2
Hanna Mudrak <annmudrak@gmail.com>
2020-05-07 17:06:17 -0500

    add test for yaml format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,10 @@ import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-test('gendiff', () => {
+test('gendiff flat json files', () => {
   expect(genDiff('__fixtures__/before.json', '__fixtures__/after.json')).toEqual(fs.readFileSync('__fixtures__/expected.txt', 'utf-8').trim());
+});
+
+test('gendiff flat yml files', () => {
+  expect(genDiff('__fixtures__/before.yml', '__fixtures__/after.yml')).toEqual(fs.readFileSync('__fixtures__/expected.txt', 'utf-8').trim());
 });
