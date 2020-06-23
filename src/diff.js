4ca2fc04aaf0a17f5591df91a490b62cb5317ec9
Hanna Mudrak <annmudrak@gmail.com>
2020-06-23 18:52:22 -0500

    changes functionality to compare nested structures

---
+++ src/diff.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+
+const makeDiff = (tree1, tree2) => {
+  const keys = _.union(Object.keys(tree1), Object.keys(tree2));
+  const result = keys.map((key) => {
+    if (!_.has(tree2, key)) {
+      return ['remove', key, tree1[key]];
+    }
+    if (!_.has(tree1, key)) {
+      return ['added', key, tree2[key]];
+    }
+    if (_.isEqual(tree1[key], tree2[key])) {
+      return ['unchanged', key, tree1[key]];
+    }
+    if (_.isObject(tree1[key]) && _.isObject(tree2[key])) {
+      return ['ancestor', key, makeDiff(tree1[key], tree2[key])];
+    }
+    return ['changed', key, tree1[key], tree2[key]];
+  });
+  return result;
+};
+
+export default makeDiff;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import parseFile from './parsers.js';
-
-const compareObjects = (obj1, obj2) => {
-  const result = [];
-  Object.keys(obj1).forEach((key) => {
-    if (key in obj2) {
-      if (obj1[key] === obj2[key]) {
-        result.push(`    ${key}: ${obj1[key]}`);
-      } else {
-        result.push(`  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`);
-      }
-    } else {
-      result.push(`  - ${key}: ${obj1[key]}`);
-    }
-  });
-  Object.keys(obj2).forEach((key) => {
-    if (!(key in obj1)) {
-      result.push(`  + ${key}: ${obj2[key]}`);
-    }
-  });
-  return `{\n${result.join('\n')}\n}`;
-};
+import makeDiff from './diff.js';
+import formatTree from './stylish.js';
 
 const genDiff = (firstPath, secondPath) => {
   const firstFile = parseFile(firstPath);
   const secondFile = parseFile(secondPath);
-
-  return compareObjects(firstFile, secondFile);
+  const diff = makeDiff(firstFile, secondFile);
+  return formatTree(diff);
 };
 
 export default genDiff;
