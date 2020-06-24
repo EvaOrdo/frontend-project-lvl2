dbdd11983ce37a7a91f6e1c7d31dc399854edbeb
Hanna Mudrak <annmudrak@gmail.com>
2020-06-24 17:32:30 -0500

    change name of prop

--- src/diff.js
+++ src/diff.js
@@ -4,7 +4,7 @@ const makeDiff = (tree1, tree2) => {
   const keys = _.union(Object.keys(tree1), Object.keys(tree2));
   const result = keys.map((key) => {
     if (!_.has(tree2, key)) {
-      return ['remove', key, tree1[key]];
+      return ['removed', key, tree1[key]];
     }
     if (!_.has(tree1, key)) {
       return ['added', key, tree2[key]];
