9c94eb43815fee02a44820b956f8b2ae514ddd5e
Hanna Mudrak <annmudrak@gmail.com>
2020-06-24 17:47:41 -0500

    add test for plain formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,18 +2,10 @@ import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-test('gendiff flat json files', () => {
-  expect(genDiff('__fixtures__/before.json', '__fixtures__/after.json')).toEqual(fs.readFileSync('__fixtures__/expected.txt', 'utf-8').trim());
-});
-
-test('gendiff flat yml files', () => {
-  expect(genDiff('__fixtures__/before.yml', '__fixtures__/after.yml')).toEqual(fs.readFileSync('__fixtures__/expected.txt', 'utf-8').trim());
-});
-
-test('gendiff flat ini files', () => {
-  expect(genDiff('__fixtures__/before.ini', '__fixtures__/after.ini')).toEqual(fs.readFileSync('__fixtures__/expected.txt', 'utf-8').trim());
-});
-
 test('gendiff tree json files', () => {
   expect(genDiff('__fixtures__/before_tree.json', '__fixtures__/after_tree.json')).toEqual(fs.readFileSync('__fixtures__/expected_tree.txt', 'utf-8').trim());
+});
+
+test('gendiff plain format tree json files', () => {
+  expect(genDiff('__fixtures__/after_tree.json', '__fixtures__/before_tree.json', 'plain')).toEqual(fs.readFileSync('__fixtures__/expected_plain_comparing.txt', 'utf-8').trim());
 });
