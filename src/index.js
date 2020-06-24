c25b861eabe572b71c5b9a399f36b7b73f58be6c
Hanna Mudrak <annmudrak@gmail.com>
2020-06-24 17:33:05 -0500

    add ability to choose format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import parseFile from './parsers.js';
 import makeDiff from './diff.js';
-import formatTree from './stylish.js';
+import chooseFormatter from './formatters/index.js';
 
-const genDiff = (firstPath, secondPath) => {
+const genDiff = (firstPath, secondPath, format) => {
   const firstFile = parseFile(firstPath);
   const secondFile = parseFile(secondPath);
   const diff = makeDiff(firstFile, secondFile);
-  return formatTree(diff);
+  return chooseFormatter(diff, format);
 };
 
 export default genDiff;
