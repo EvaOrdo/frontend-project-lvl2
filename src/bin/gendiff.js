648b388c88cd7cc8a44a317f8415ec3cb2b2c7e1
Hanna Mudrak <annmudrak@gmail.com>
2020-06-24 17:36:31 -0500

    add default format and format as arg to genDiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -9,8 +9,8 @@ program
   .version('0.0.1')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f --format [type]', 'output format')
+  .option('-f --format [type]', 'output format', 'recursive')
   .action((firstConfig, secondConfig) => {
-    console.log(genDiff(firstConfig, secondConfig));
+    console.log(genDiff(firstConfig, secondConfig, program.format));
   });
 program.parse(process.argv);
