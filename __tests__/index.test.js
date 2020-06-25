b8d963c6b38f5d584edebd384e1535d53eb3835e
Hanna Mudrak <annmudrak@gmail.com>
2020-06-24 21:03:14 -0500

    add test for json format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,3 +9,7 @@ test('gendiff tree json files', () => {
 test('gendiff plain format tree json files', () => {
   expect(genDiff('__fixtures__/after_tree.json', '__fixtures__/before_tree.json', 'plain')).toEqual(fs.readFileSync('__fixtures__/expected_plain_comparing.txt', 'utf-8').trim());
 });
+
+test('gendiff json format tree json files', () => {
+  expect(genDiff('__fixtures__/before_tree.json', '__fixtures__/after_tree.json', 'json')).toEqual(fs.readFileSync('__fixtures__/expected_json_format.txt', 'utf-8').trim());
+});
