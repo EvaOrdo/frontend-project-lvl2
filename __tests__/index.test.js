f4469f33054cc6a13a13fd3575b5ee35e880c8fc
Hanna Mudrak <annmudrak@gmail.com>
2020-06-29 13:40:48 -0500

    add tests for ini and yml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,14 +2,20 @@ import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-test('gendiff tree json files', () => {
-  expect(genDiff('__fixtures__/before_tree.json', '__fixtures__/after_tree.json')).toEqual(fs.readFileSync('__fixtures__/expected_tree.txt', 'utf-8').trim());
+test('gendif recursive format', () => {
+  expect(genDiff('__fixtures__/before.json', '__fixtures__/after.json')).toEqual(fs.readFileSync('__fixtures__/recursive.txt', 'utf-8').trim());
+  expect(genDiff('__fixtures__/before.yml', '__fixtures__/after.yml')).toEqual(fs.readFileSync('__fixtures__/recursive.txt', 'utf-8').trim());
+  expect(genDiff('__fixtures__/before.ini', '__fixtures__/after.ini')).toEqual(fs.readFileSync('__fixtures__/recursive.txt', 'utf-8').trim());
 });
 
-test('gendiff plain format tree json files', () => {
-  expect(genDiff('__fixtures__/after_tree.json', '__fixtures__/before_tree.json', 'plain')).toEqual(fs.readFileSync('__fixtures__/expected_plain_comparing.txt', 'utf-8').trim());
+test('gendiff plain format', () => {
+  expect(genDiff('__fixtures__/before.json', '__fixtures__/after.json', 'plain')).toEqual(fs.readFileSync('__fixtures__/plain.txt', 'utf-8').trim());
+  expect(genDiff('__fixtures__/before.yml', '__fixtures__/after.yml', 'plain')).toEqual(fs.readFileSync('__fixtures__/plain.txt', 'utf-8').trim());
+  expect(genDiff('__fixtures__/before.ini', '__fixtures__/after.ini', 'plain')).toEqual(fs.readFileSync('__fixtures__/plain.txt', 'utf-8').trim());
 });
 
-test('gendiff json format tree json files', () => {
-  expect(genDiff('__fixtures__/before_tree.json', '__fixtures__/after_tree.json', 'json')).toEqual(fs.readFileSync('__fixtures__/expected_json_format.txt', 'utf-8').trim());
+test('gendiff json format', () => {
+  expect(genDiff('__fixtures__/before.json', '__fixtures__/after.json', 'json')).toEqual(fs.readFileSync('__fixtures__/json.txt', 'utf-8').trim());
+  expect(genDiff('__fixtures__/before.yml', '__fixtures__/after.yml', 'json')).toEqual(fs.readFileSync('__fixtures__/json.txt', 'utf-8').trim());
+  expect(genDiff('__fixtures__/before.ini', '__fixtures__/after.ini', 'json')).toEqual(fs.readFileSync('__fixtures__/json.txt', 'utf-8').trim());
 });
