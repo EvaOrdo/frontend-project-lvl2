d9b1003961b24d16a5b2b7e5fc997e19a7f2ef3a
Hanna Mudrak <annmudrak@gmail.com>
2020-07-13 18:14:44 -0500

    refactoring: change diff tree from arrs to objs

--- src/diff.js
+++ src/diff.js
@@ -4,18 +4,20 @@ const makeDiff = (tree1, tree2) => {
   const keys = _.union(Object.keys(tree1), Object.keys(tree2));
   const result = keys.map((key) => {
     if (!_.has(tree2, key)) {
-      return ['removed', key, tree1[key]];
+      return { status: 'removed', key, val1: tree1[key] };
     }
     if (!_.has(tree1, key)) {
-      return ['added', key, tree2[key]];
+      return { status: 'added', key, val1: tree2[key] };
     }
     if (_.isEqual(tree1[key], tree2[key])) {
-      return ['unchanged', key, tree1[key]];
+      return { status: 'unchanged', key, val1: tree1[key] };
     }
     if (_.isObject(tree1[key]) && _.isObject(tree2[key])) {
-      return ['ancestor', key, makeDiff(tree1[key], tree2[key])];
+      return { status: 'ancestor', key, children: makeDiff(tree1[key], tree2[key]) };
     }
-    return ['changed', key, tree1[key], tree2[key]];
+    return {
+      status: 'changed', key, val1: tree1[key], val2: tree2[key],
+    };
   });
   return result;
 };
